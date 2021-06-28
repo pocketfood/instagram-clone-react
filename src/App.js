cb44f9bdc7da4eb07e77920edab21805e7955f81
Matt <matt@metapacific.xyz>
2021-06-27 21:05:00 -0400

    first push

---
+++ src/App.js
@@ -0,0 +1,21 @@
+  // src.App.js
+
+  import React, { Component } from 'react';
+  import './App.css';
+  import Header from './components/Header';
+  import Post from './components/Post';
+  
+  class App extends Component {
+    render() {
+      return (
+        <div>
+          <Header />
+          <div>
+            <Post />
+          </div>
+        </div>
+      );
+    }
+  }
+  export default App;
+  
